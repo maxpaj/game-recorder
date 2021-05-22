b5f127b4d012f8434510dcaf3889e84f06dc44e9
Maximilian Paju <me@maxpaj.com>
2021-05-22 15:57:47 +0200

    rename

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { GameConsole } from "./console";
 import { MemoryReader } from "./memory_reader";
-import { OBSRecorder } from "./obs-recorder";
+import { OBSRecorder } from "./obs_recorder";
 
 export {
     GameConsole,
